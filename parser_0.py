bd97641819f346634c94525177d7833061f32ded
Jeremie Maufret <sayou.jeremie@gmail.com>
2021-03-31 23:35:01 +0200

    optimisation variables, arrayInfo

--- parser_0.py
+++ parser_0.py
@@ -1,76 +1,78 @@
 from bs4 import BeautifulSoup
 import urllib.request
 from datetime import datetime
-from url import tab_url
+from url import urlArraySeries
 
 now = datetime.now()
 
-urlpage = tab_url[0]
-page = urllib.request.urlopen(urlpage)
+urlPage = urlArraySeries[0]
+page = urllib.request.urlopen(urlPage)
 soup = BeautifulSoup(page, 'html.parser')
-block_a = soup.select("div h3 a")
+urlLinks = soup.select("div h3 a")
 
-tab_test = []
-list_volumes = []
-tab_serie = []
+numberArrayVolumesBySerie = []
+arrayVolumes = []
+arraySeries = []
+informationsVolume = []
+arrayTemporary = []
 
-for link_serie in block_a:
-    page_serie = urllib.request.urlopen(link_serie)
-    serie = BeautifulSoup(page_serie, 'html.parser')
+for linkSerie in urlLinks:
+    urlSerie = urllib.request.urlopen(linkSerie.get('href'))
+    serie = BeautifulSoup(urlSerie, 'html.parser')
     title = (serie.find("div", id="originaltitle")).get_text()
-    block_volumes = serie.find_all('div', class_= "series-volume")
-    tab_test.append(len(block_volumes))
-    tab_serie.append(title)
-    for link_volume in block_volumes:
-        list_volumes.append((link_volume.find('a')).get('href'))
-
-detail_volume = []
-temporary = []
+    arrayVolumesBySerie = serie.find_all('div', class_= "series-volume")
+    numberArrayVolumesBySerie.append(len(arrayVolumesBySerie))
+    arraySeries.append(title)
+    for linkVolumes in arrayVolumesBySerie:
+        arrayVolumes.append((linkVolumes.find('a')).get('href'))
 
 fichier = open("volume_detail_"+now.strftime("%d-%m-%Y-%H%M%S")+".txt", "a+b")
 
 a = b = c = 0 
 
-tab_info = []
-for i in range(len(list_volumes)):
+arrayInfo = []
+for i in range(len(arrayVolumes)):
     if b == 0:
-        fichier.write((tab_serie[c]).encode('utf-8')+b'\n')
-    page_volume = urllib.request.urlopen(list_volumes[i])
-    volume = BeautifulSoup(page_volume, 'html.parser')
+        fichier.write((arraySeries[c]).encode('utf-8')+b'\n')
+
+    linkPageVolume = urllib.request.urlopen(arrayVolumes[i])
+    volume = BeautifulSoup(linkPageVolume, 'html.parser')
     fichier.write((volume.title.string).encode('utf-8')+b'\n')
-    block_volume = volume.find('div', id='volume-meta')
-    content_p = block_volume.find('p')
-    temporary.extend([list_volumes[i], content_p])
-    detail_volume.append(temporary)
+    contentBlockVolume = (volume.find('div', id='volume-meta')).find('p')
+    arrayTemporary.extend([arrayVolumes[i], contentBlockVolume])
+    informationsVolume.append(arrayTemporary)
 
-    tab_info.append((detail_volume[0][1].find('a')).get('href'))
-    author = (detail_volume[0][1].find('a')).getText()
-    tab_info.append(author)
-    release = str(str(detail_volume[0][1]).split('<b>Release Date:</b> ')[1]).split(' <b>')
-    tab_info.append(release[0])
-    price = str(str(detail_volume[0][1]).split('<b>Price:</b> ')[1]).split(' <b>')
-    tab_info.append(price[0])
-    format_serie = str(str(detail_volume[0][1]).split('<b>Format:</b> ')[1]).split(' <b>')
-    tab_info.append(format_serie[0])
-    trim = str(str(detail_volume[0][1]).split('<b>Trim:</b> ')[1]).split(' <b>')
-    tab_info.append(trim[0])
-    page_count = str(str(detail_volume[0][1]).split('<b>Page Count:</b> ')[1]).split(' <b>')
-    tab_info.append(page_count[0])
-    isbn = str(str(detail_volume[0][1]).split('<b>ISBN:</b> ')[1]).split('</p>')
-    tab_info.append(isbn[0])
-    fichier.write(detail_volume[0][0].encode('utf-8')+b'\n')
+    #link author
+    arrayInfo.append((informationsVolume[0][1].find('a')).get('href'))
+    #author
+    arrayInfo.append((informationsVolume[0][1].find('a')).getText())
+    #release
+    arrayInfo.append((str(str(informationsVolume[0][1]).split('<b>Release Date:</b> ')[1]).split(' <b>'))[0])
+    #price
+    arrayInfo.append((str(str(informationsVolume[0][1]).split('<b>Price:</b> ')[1]).split(' <b>'))[0])
+    #format serie
+    arrayInfo.append((str(str(informationsVolume[0][1]).split('<b>Format:</b> ')[1]).split(' <b>'))[0])
+    #dimension
+    arrayInfo.append((str(str(informationsVolume[0][1]).split('<b>Trim:</b> ')[1]).split(' <b>'))[0])
+    #page count
+    arrayInfo.append((str(str(informationsVolume[0][1]).split('<b>Page Count:</b> ')[1]).split(' <b>'))[0])
+    #isbn
+    arrayInfo.append((str(str(informationsVolume[0][1]).split('<b>ISBN:</b> ')[1]).split('</p>'))[0])
+    fichier.write(informationsVolume[0][0].encode('utf-8')+b'\n')
 
-    for info in tab_info:
+    for info in arrayInfo:
         fichier.write(info.encode('utf-8')+b'\n')
-    fichier.write(b'\n')
 
-    temporary[:]  = []
-    tab_info[:]  = []
-    if b + 1 == tab_test[a]:
+    fichier.write(b'\n')
+    arrayTemporary[:]  = []
+    arrayInfo[:]  = []
+
+    if b + 1 == numberArrayVolumesBySerie[a]:
         fichier.write(b'\n')
         a = a + 1
         b = 0
         c = c + 1
     else:
-        b = b + 1 
+        b = b + 1
+
 fichier.close()
