24330d6f0389b1d78b2774fa72c560823705c708
Jeremie Maufret <sayou.jeremie@gmail.com>
2021-04-06 23:24:40 +0200

    feature json

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # python_parsing_books
 
 Parsing with BeautifulSoup
+
+pip install beautifulsoup4
+pip install repackage
--- parsers/parser_0.py
+++ parsers/parser_0.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
 import urllib.request
-from datetime import datetime
+import json
 import repackage
 repackage.up()
 from url import urlArraySeries
-
-now = datetime.now()
 
 urlPage = urlArraySeries[0]
 page = urllib.request.urlopen(urlPage)
@@ -16,6 +14,8 @@ numberArrayVolumesBySerie = []
 arrayVolumes = []
 arraySeries = []
 informationsVolume = []
+a = b = 0 
+dict = {}
 
 for linkSerie in urlLinks:
     urlSerie = urllib.request.urlopen(linkSerie.get('href'))
@@ -24,56 +24,38 @@ for linkSerie in urlLinks:
     arrayVolumesBySerie = serie.find_all('div', class_= "series-volume")
     numberArrayVolumesBySerie.append(len(arrayVolumesBySerie))
     arraySeries.append(title)
+
     for linkVolumes in arrayVolumesBySerie:
         arrayVolumes.append((linkVolumes.find('a')).get('href'))
 
-fichier = open("volume_detail_"+now.strftime("%d-%m-%Y-%H%M%S")+".txt", "a+b")
-
-a = b = c = 0 
-
-arrayInfo = []
 for i in range(len(arrayVolumes)):
-    if b == 0:
-        fichier.write((arraySeries[c]).encode('utf-8')+b'\n')
-
     linkPageVolume = urllib.request.urlopen(arrayVolumes[i])
     volume = BeautifulSoup(linkPageVolume, 'html.parser')
-    fichier.write((volume.title.string).encode('utf-8')+b'\n')
     contentBlockVolume = (volume.find('div', id='volume-meta')).find('p')
     informationsVolume.extend([arrayVolumes[i], contentBlockVolume])
 
-    #link author
-    arrayInfo.append((informationsVolume[1].find('a')).get('href'))
-    #author
-    arrayInfo.append((informationsVolume[1].find('a')).getText())
-    #release
-    arrayInfo.append((str(str(informationsVolume[1]).split('<b>Release Date:</b> ')[1]).split(' <b>'))[0])
-    #price
-    arrayInfo.append((str(str(informationsVolume[1]).split('<b>Price:</b> ')[1]).split(' <b>'))[0])
-    #format serie
-    arrayInfo.append((str(str(informationsVolume[1]).split('<b>Format:</b> ')[1]).split(' <b>'))[0])
-    #dimension
-    arrayInfo.append((str(str(informationsVolume[1]).split('<b>Trim:</b> ')[1]).split(' <b>'))[0])
-    #page count
-    arrayInfo.append((str(str(informationsVolume[1]).split('<b>Page Count:</b> ')[1]).split(' <b>'))[0])
-    #isbn
-    arrayInfo.append((str(str(informationsVolume[1]).split('<b>ISBN:</b> ')[1]).split('</p>'))[0])
-    
-    fichier.write(informationsVolume[0].encode('utf-8')+b'\n')
+    dict[i] = {
+        'id': i,
+        'title' : arraySeries[a],
+        'volume' : ((volume.title.string).split('  |  '))[0],
+        'editor' : ((volume.title.string).split('  |  '))[1],
+        'link author': (informationsVolume[1].find('a')).get('href'),
+        'author' : (informationsVolume[1].find('a')).getText(),
+        'release' : ((str(informationsVolume[1]).split('<b>Release Date:</b> ')[1]).split(' <b>'))[0],
+        'price' : ((str(informationsVolume[1]).split('<b>Price:</b> ')[1]).split(' <b>'))[0],
+        'format' : ((str(informationsVolume[1]).split('<b>Format:</b> ')[1]).split(' <b>'))[0],
+        'dimension' : ((str(informationsVolume[1]).split('<b>Trim:</b> ')[1]).split(' <b>'))[0],
+        'page count' : ((str(informationsVolume[1]).split('<b>Page Count:</b> ')[1]).split(' <b>'))[0],
+        'isbn' : ((str(informationsVolume[1]).split('<b>ISBN:</b> ')[1]).split('</p>'))[0]
+    }
 
-    for info in arrayInfo:
-        fichier.write(info.encode('utf-8')+b'\n')
-
-    fichier.write(b'\n')
     informationsVolume[:] = []
-    arrayInfo[:] = []
 
     if b + 1 == numberArrayVolumesBySerie[a]:
-        fichier.write(b'\n')
         a = a + 1
         b = 0
-        c = c + 1
     else:
         b = b + 1
 
-fichier.close()
+with open('SevenSeas.json', 'w', encoding='utf-8') as write_file:
+    json.dump(dict, write_file, ensure_ascii=False, indent=4)
