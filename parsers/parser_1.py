98ba2efb06f06458376904d2cec6adf594546a3e
Jeremie Maufret <sayou.jeremie@gmail.com>
2021-04-11 22:46:31 +0200

    modifications écriture json parser_1

--- parsers/parser_1.py
+++ parsers/parser_1.py
@@ -12,7 +12,6 @@ from url import urlArraySeries, urlDomain, namePublisher
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 start = time.time()
 
-
 urlPage = urlArraySeries[1]
 page = urlopen(Request(urlPage, headers=headers)).read()
 soup = BeautifulSoup(page, 'html.parser')
@@ -30,8 +29,6 @@ for links in blocksSeries:
 arrayLinksVolume = []
 linksTemporary = []
 dict = {}
-
-
 
 for links in arrayLinks:
     urlSerie = urlopen(Request(links.replace('\n&', '&'), headers=headers)).read()
@@ -54,20 +51,27 @@ for i in range(len(arrayLinksVolume)):
     pageVolume = BeautifulSoup(urlSerie, 'html.parser')
 
     title = (((str(pageVolume.find('h2', id='book-title')).split('>'))[1]).split('<'))[0]
-
     if re.search(', Vol. ', title):
         tempTitle = (title.split(', Vol. '))[0]
     else:
         tempTitle = 'Undefined'
 
     typeOfBook = 'Undefined'
-
     if re.search('light novel', title):
         typeOfBook = 'Light Novel'
-
     if re.search('manga', title):
         typeOfBook = 'Manga'
-    
+
+    if (len(pageVolume.find_all('span', class_='detail-value')) > 3):
+        pages = (str(pageVolume.find_all('span', class_='detail-value')[3]))
+    else:
+        pages = 'Undefined'
+
+    if re.search(' Pages<span>', pages):
+        tempPages = ((pageVolume.find_all('span', class_='detail-value'))[3]).getText()
+    else:
+        tempPages = 'Undefined'
+
     dict[i] = {
         'id' : i,
         'title' : tempTitle,
@@ -75,17 +79,18 @@ for i in range(len(arrayLinksVolume)):
         'type of book' : typeOfBook,
         'author' : (pageVolume.find('h3', id='book-author')).getText(),
         'format book' : ((((pageVolume.find_all('span', class_='detail-key'))[0]).getText()).split('Format: '))[1],
-        'page' : ((pageVolume.find_all('span', class_='detail-value'))[3]).getText(),
+        'pages' : tempPages,
         'isbn' : ((pageVolume.find_all('span', class_='detail-value'))[0]).getText(),
         'imprit' : ((pageVolume.find_all('span', class_='detail-value'))[1]).getText(),
         'price' : ((((pageVolume.find_all('span', class_='detail-key'))[3]).getText()).split('Price: '))[1],
         'date_mm/dd/yyyy' : ((pageVolume.find_all('span', class_='detail-value'))[2]).getText(),
-        'genres' : ((str(pageVolume.find('div', id='book-categories')).split('</h4>'))[1].split('<h4>'))[0].replace('\n', ''),
-        'subgenres' : ((str(pageVolume.find('div', id='book-categories')).split('</h4>'))[2].split('</div>'))[0].replace('\n', '')
+        'genres' : (((str(pageVolume.find('div', id='book-categories')).split('</h4>'))[1].split('<h4>'))[0].replace('\n', '')).replace('&amp;', '&'),
+        'subgenres' : (((str(pageVolume.find('div', id='book-categories')).split('</h4>'))[2].split('</div>'))[0].replace('\n', '')).replace('&amp;', '&')
     }
 
 stop = time.time()
 print("The time of the run:", stop - start, "s")
+#The time of the run: 5390.788335323334 s
 
 with open(namePublisher[1]+'.json', 'w', encoding='utf-8') as write_file:
     json.dump(dict, write_file, ensure_ascii=False, indent=4)
